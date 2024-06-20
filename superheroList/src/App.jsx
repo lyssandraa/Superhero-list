fe3cc40b091298683044b9777eaefcd65283d9a2
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-20 10:57:33 +0100

    added hero card

--- superheroList/src/App.jsx
+++ superheroList/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { allHeroes } from "./HeroData";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  return (
+    <div>
+      <h1>My Superhero Wiki</h1>
 
+      {allHeroes.map((heroInfo, index) => {
+        return <HeroCard key={index} heroObj={heroInfo} />;
+      })}
+    </div>
+  );
+};
+
+const HeroCard = (props) => {
   return (
     <>
+      <p>HERO: {props.heroObj.hero}</p>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>INFO: {props.heroObj.info}</p>
+        <p>VILLAIN: {props.heroObj.villain}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

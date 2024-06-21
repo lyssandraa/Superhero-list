8fa543b8e2e82322a89cb78e4d7be7a846a89979
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-21 10:59:15 +0100

    added functions and buttons

--- superheroList/src/App.jsx
+++ superheroList/src/App.jsx
@@ -3,13 +3,58 @@ import "./App.css";
 import { allHeroes } from "./HeroData";
 
 const App = () => {
+  const [favourites, setFavourites] = useState([]);
+
+  const handleAddToFaves = (hero) => {
+    let newFaveArr = [...favourites];
+    newFaveArr.push(hero);
+    setFavourites(newFaveArr);
+  };
+
+  const handleRemove = (index) => {
+    let newFaveArr = [...favourites];
+    newFaveArr.splice(index, 1);
+    setFavourites(newFaveArr);
+  };
+
   return (
     <div>
       <h1>My Superhero Wiki</h1>
 
-      {allHeroes.map((heroInfo, index) => {
-        return <HeroCard key={index} heroObj={heroInfo} />;
-      })}
+      <div>
+        <h3>My Favourite Heroes</h3>
+        {favourites.map((faveHero, index) => {
+          return (
+            <Favourites
+              key={index}
+              faveHeroData={faveHero}
+              removeFave={() => handleRemove(index)}
+            />
+          );
+        })}
+      </div>
+
+      <div>
+        <h3>All Heroes</h3>
+        {allHeroes.map((heroInfo, index) => {
+          return (
+            <HeroCard
+              key={index}
+              heroObj={heroInfo}
+              faveFunc={handleAddToFaves}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Favourites = ({ faveHeroData, removeFave }) => {
+  return (
+    <div>
+      <p>{faveHeroData.hero}</p>
+      <button onClick={removeFave}>X</button>
     </div>
   );
 };
@@ -26,7 +71,15 @@ const HeroCard = (props) => {
           <p>VILLAIN: {props.heroObj.villain}</p>
         </div>
       )}
-      <button onClick={() => setShow(!show)}>SHOW INFO</button>
+
+      {/* if there is something passed as an argument in brackets, it has to be given an anonymous arrow syntax function */}
+      <button onClick={() => setShow(!show)}>
+        {show ? "HIDE INFO" : "SHOW INFOR"}
+      </button>
+
+      <button onClick={() => props.faveFunc(props.heroObj)}>
+        Add to Faves
+      </button>
     </>
   );
 };
